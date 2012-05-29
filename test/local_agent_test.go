2f2b6b35507b14ff27c279d406b001930e8c1a30
Matthew Kanwisher <matt@kanwisher.com>
2012-05-29 15:30:46 -0400

    starting the debian package files

---
+++ test/local_agent_test.go
@@ -0,0 +1,21 @@
+package errplane_tests
+
+import (
+  "errplane"
+  "testing"
+  "launchpad.net/gocheck"
+)
+
+//http://labix.org/gocheck
+
+func Test(t *testing.T) { TestingT(t)}
+
+type LocalAgentSuite struct{}
+var _ = Suite(&LocalAgentSuite{})
+
+func (s *LocalAgentSuite) TestShouldRequestFoo(c *C) {
+  data, err errplane.dummy_method
+
+  c.Check(err, IsNil)
+  c.Assert(string(data), Equals, "This is Foo handler!")
+}
