e1e0c26898b1ce4237fff8e16f416ef8e56dabfe
Matthew Kanwisher <matt@kanwisher.com>
2012-05-29 17:05:12 -0400

    local agent

--- build_deb.sh
+++ build_deb.sh
@@ -1,6 +1,7 @@
-/var/lib/jenkins/bin/go build src/local_agent.go
+PATH=$PATH:/var/lib/jenkins/bin/
+go build src/local_agent.go
 go install launchpad.net/gocheck
-go install 
+go install github.com/kless/goconfig/config
 cp local_agent packages/deb_pkg/errplane/usr/local/bin/errplane-local-agent
 cd packages/deb_pkg
 dpkg --build errplane ./
