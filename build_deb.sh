074efc6fb568d601d3a42b4c81bc4eeb08553630
Matthew Kanwisher <matt@kanwisher.com>
2012-05-29 17:12:16 -0400

    local agent

--- build_deb.sh
+++ build_deb.sh
@@ -1,6 +1,6 @@
-GOROOT=/var/lib/jenkins
-GOBIN=/var/lib/jenkins/bin
-PATH=$PATH:/var/lib/jenkins/bin
+GOROOT=/var/lib/jenkins/go/bin
+GOBIN=/var/lib/jenkins/go/bin
+PATH=$PATH:/var/lib/jenkins/go/bin
 go build src/local_agent.go
 go install launchpad.net/gocheck
 go install github.com/kless/goconfig/config
