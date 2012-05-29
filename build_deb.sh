c71a5423b4482ff45a43145827971768ddf80120
Matthew Kanwisher <matt@kanwisher.com>
2012-05-29 17:02:18 -0400

    finishing go

--- build_deb.sh
+++ build_deb.sh
@@ -1,4 +1,4 @@
-go build src/local_agent.go
+/var/lib/jenkins/bin/go build src/local_agent.go
 cp local_agent packages/deb_pkg/errplane/usr/local/bin/errplane-local-agent
 cd packages/deb_pkg
 dpkg --build errplane ./
