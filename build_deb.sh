af941f24fc0ed9b338e18a9baa62dc3a1a40db4e
Matthew Kanwisher <matt@kanwisher.com>
2012-05-30 14:25:31 -0400

    fixing sed

--- build_deb.sh
+++ build_deb.sh
@@ -2,7 +2,7 @@ CONTROL=packages/deb_pkg/errplane/DEBIAN/control
 GOROOT=/var/lib/jenkins/go
 GOBIN=/var/lib/jenkins/bin
 PATH=$PATH:/var/lib/jenkins/bin
-sed "s/_BUILD_/${BUILD_NUMBER}/g" $CONTROL > $CONTROL
+sed -i "s/_BUILD_/${BUILD_NUMBER}/g" $CONTROL 
 go build src/local_agent.go
 go get launchpad.net/gocheck
 go get github.com/kless/goconfig/config
