1bac21d9d56428aea480c6f6eb1225a3cc69ae42
Matthew Kanwisher <matt@kanwisher.com>
2012-06-10 15:35:20 -0400

    fixing jenkins

--- build_deb.sh
+++ build_deb.sh
@@ -8,10 +8,10 @@ rm ./packages/deb_pkg/errplane-local-agent*
 #requires gnu sed 
 sed -i "s/_BUILD_/${BUILD_NUMBER}/g" $CONTROL 
 sed -i "s/_BUILD_/${BUILD_NUMBER}/g" $VER_SRC 
-go build src/local_agent.go
 go get launchpad.net/gocheck
 go get github.com/kless/goconfig/config
 go get github.com/droundy/goopt
+go build src/local_agent.go
 #cp local_agent packages/deb_pkg/errplane/usr/local/bin/errplane-local-agent
 rm packages/deb_pkg/errplane/usr/local/bin/errplane-local-agent
 cp local_agent packages/deb_pkg/errplane-local-agent-${BUILD_NUMBER}
